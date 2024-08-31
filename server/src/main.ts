36c04c8f47e0a454bb78a26765f5463fcfa67220
Ryan Quinn <ryanwilliamquinn@gmail.com>
2024-08-30 22:23:08 -0700

    add port from config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT') || 3000;
+  console.log(`Server running on http://localhost:${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
